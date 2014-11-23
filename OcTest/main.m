2ecc7b8503a833588670bc681a3d578ddae77797
刘文明 <scott@Scott-MacBook-Pro.local>
2014-11-23 21:18:58 +0800

    Initial Commit

---
+++ OcTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OcTest
+//
+//  Created by 刘文明 on 14/11/23.
+//  Copyright (c) 2014年 ScottStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
